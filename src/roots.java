025d568d9907d241993c248bf0505f42a758f8b0
Preet Monga <131587235+PreetMongaPM@users.noreply.github.com>
2024-04-27 13:34:02 +0530

    Add files via upload

---
+++ src/roots.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class roots {
+    public static void main()
+    {
+        int a,b,c;
+        System.out.println("Enter the coefficients a,b,c of quadratic equations");
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+    }
+}
